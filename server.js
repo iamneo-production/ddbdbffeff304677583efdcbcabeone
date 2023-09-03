4e43a8974b878c38fb717bc0ce472ffd138e4e26
Iamneo <techadmin@iamneo.ai>
2023-09-03 13:22:22 +0000

    Backup Commit

--- server.js
+++ server.js
@@ -10,5 +10,26 @@ const container = document.getElementById("gameContainer");
 const squareArray = [];
 let nextMove = "X";
 function gameOver(message){
-    document.getElementById("winner")
+    document.getElementById("winner").innerHTML=message;
+    container.style.display="none";
+    document.getElementById("gameOver").style.display="block";
+
+}
+function isDraw(){
+    let shouldReturn = true;
+    squareArray.forEach(({state})=>{
+        if(state=="")
+        shouldReturn=false;
+    });
+    return shouldReturn;
+}
+function wonGame(){
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2]
+    ]
 }
