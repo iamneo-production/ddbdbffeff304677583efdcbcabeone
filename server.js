c8940d232ebab9fcd7813e9ca693ff0ed66d451e
Iamneo <techadmin@iamneo.ai>
2023-09-03 13:17:22 +0000

    Backup Commit

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const port = 8081;
 //app.use('/static', express.static('public'))
 app.use(express.static('public'))
 app.listen(port, () => console.log(`listening on port ${port}!`));
+
+const container = document.getElementById("gameContainer");
+const squareArray = [];
+let nextMove = "X";
+function gameOver(message){
+    document.getElementById("winner")
+}
